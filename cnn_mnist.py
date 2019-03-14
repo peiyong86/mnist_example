c029a6f432fe3339340e98f60948f79a00d3370d
kevinpei <kevinpei@webank.com>
2019-03-14 19:42:41 +0800

    Feat: add train and eval code, with baseline model.

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -10,6 +10,9 @@ import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.INFO)
 
 
+N_CLASS = 10
+
+
 def load_data():
     # Load training and eval data
     mnist = tf.contrib.learn.datasets.load_dataset("mnist")
@@ -22,6 +25,37 @@ def load_data():
     assert len(eval_data)==10000
 
     return train_data, train_labels, eval_data, eval_labels
+
+
+def init_model():
+    # create model and return
+    classifier = tf.estimator.BaselineClassifier(n_classes=N_CLASS)
+    return classifier
+
+
+def main(argv):
+    # init model
+    model = init_model()
+    # load data
+    train_data, train_labels, eval_data, eval_labels = load_data()
+    # train input fn
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": train_data},
+    y=train_labels,
+    batch_size=100,
+    num_epochs=1,
+    shuffle=True)
+    # train model
+    model.train(input_fn=train_input_fn)
+    # eval model
+    # Evaluate the model and print results
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False)
+    eval_results = model.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
 
 
 if __name__ == '__main__':
